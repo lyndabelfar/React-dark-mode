f7323302756a8f9434f55f81f97d6952b0604976
lyndabelfar <lyndabelar@gmail.com>
2022-09-04 00:14:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [isDark, setIsDark] = useState(false)
+  function handleButtonClick(){
+    setIsDark(!isDark)
+  }
+  useEffect(()=>{
+    document.body.classList.toggle('dark')
+  },[isDark])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-top">
+        <h2 className={isDark?'dark':''}>Overreacted</h2>
+        <button className={isDark?'dark':''} onClick={handleButtonClick}>{isDark?'light mode':'dark mode'}</button>
+      </div>
+      <div className="articles">
+        <div className="article-1">
+          <h1 className={isDark?'dark':''}>The WET Codebase</h1>
+          <b className={isDark?'dark':''}>Sunday 4th, 202011 min read</b>
+          <div className={isDark?'detail dark':'detail'}>Come waste your time with me</div>
+        </div>
+        <div className="article-2">
+          <h1 className={isDark?'dark':''}>Goodby, Clean Code</h1>
+          <b className={isDark?'dark':''}>Friday 22nd, 20195 min read</b>
+          <div className={isDark?'detail dark':'detail'}>Let clean code guide you. Then let it go.</div>
+        </div>
+        <div className="article-3">
+          <h1 className={isDark?'dark':''}>My Decade In Review</h1>
+          <b className={isDark?'dark':''}>Saturday 11th, 20185 min read</b>
+          <div className={isDark?'detail dark':'detail'}>A personal reflection.</div>
+        </div>
+        <div className="article-4">
+          <h1 className={isDark?'dark':''}>What Are The React Team Principles</h1>
+          <b className={isDark?'dark':''}>Thursday 4th, 20155 min read</b>
+          <div className={isDark?'detail dark':'detail'}>UI Before API.</div>
+        </div>
+      </div>
+
     </div>
   );
 }
